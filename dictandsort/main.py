351c813e3988dcb967aa6a139f2e26140a6111d3
Rahul Saini <rscoder7@gmail.com>
2024-08-09 10:34:46 +1000

    added sorting code properly

--- dictandsort/main.py
+++ dictandsort/main.py
@@ -10,11 +10,20 @@ def read_clothing_data(filename):
 
 def sort_clothing(clothing_data, sort_choice):
 
+    if sort_choice == 1:
+        sorted_data = sorted(clothing_data.items(), key=lambda x: x[1][0])
+    elif sort_choice == 2:
+        sorted_data = sorted(clothing_data.items(), key=lambda x: x[1][1])
+    else:
+        sorted_data = sorted(clothing_data.items(), key=lambda x: x[1][2])
+    return sorted_data
 
 def main():
     filename = "clothing.csv"
     clothing_data = read_clothing_data(filename)
 
+    sort_choice = int(input("What would you like to sort on?\ntype - 1, colour - 2, size - 3: "))
+    print(f"You are sorting on the {['type', 'colour', 'size'][sort_choice - 1]} of clothing.")
 
     sorted_data = sort_clothing(clothing_data, sort_choice)
     for item in sorted_data:
