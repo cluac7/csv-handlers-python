d23da2a417447983d51dc4c765ee39f8e9887322
Rahul Saini <rscoder7@gmail.com>
2024-08-09 12:26:55 +1000

    add sharing check logic

--- cakes/main.py
+++ cakes/main.py
@@ -7,5 +7,14 @@ def read_cake_data(filename):
         for row in reader:
             cake_choice = row['choice']
             friend = row['friend']
+            if cake_choice not in cake_data:
+                cake_data[cake_choice] = set()
+            cake_data[cake_choice].add(friend)
     return cake_data
+
+def find_cake_sharers(cake_data, cake_of_the_day):
+    if cake_of_the_day in cake_data:
+        return cake_data[cake_of_the_day]
+    else:
+        return set()
 
