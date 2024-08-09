05380e311014ae04d2fb021dee2fc0cd6bf58de1
Rahul Saini <rscoder7@gmail.com>
2024-08-09 12:29:40 +1000

    added input for the user

--- cakes/main.py
+++ cakes/main.py
@@ -18,3 +18,18 @@ def find_cake_sharers(cake_data, cake_of_the_day):
     else:
         return set()
 
+def main():
+    filename = "cake.csv"
+    cake_data = read_cake_data(filename)
+
+    cake_of_the_day = input("What is the cake of the day? ")
+    sharers = find_cake_sharers(cake_data, cake_of_the_day)
+
+    if sharers:
+        print(f"Set of friends to share the cake: {sorted(sharers)}")
+    else:
+        print(f"{cake_of_the_day} cake is all yours!")
+
+if __name__ == "__main__":
+    main()
+
