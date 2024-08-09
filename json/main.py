e560499ef86760ceee42f251410c3b82e0b54ab1
Rahul Saini <rscoder7@gmail.com>
2024-08-09 14:31:36 +1000

    handle csv

--- json/main.py
+++ json/main.py
@@ -2,9 +2,14 @@ import csv
 import json
 
 def combine_and_calculate(movies_csv, reviews_json):
+  movie_data = {}
+  with open(movies_csv, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      movie_data[row['id']] = row['title']
+
 
 def print_results(combined_data):
-  print("Movie ratings:")
 
 if __name__ == "__main__":
   movies_csv = "movies.csv"
